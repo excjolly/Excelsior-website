314d8214d2d57993866c7da36dc260695d420cb1
excjolly <sagar@getexcelsior.com>
2022-05-07 17:57:28 +0530

    new pages content

--- src/components/Disclaimer/index.js
+++ src/components/Disclaimer/index.js
@@ -5,7 +5,13 @@ const Disclaimer = () => {
     <div>
         <div className='container mt-4'>
 				<h1 className='Banner_Heading'>EXCELSIOR</h1>
-				<h3 className='mb-4 '>We don't just train, We make careers</h3>
+				<h3 className='mb-4 '>Disclaimer</h3>
+        <p> Excelsior's Platforms and/or Programs may contain typographical errors or inaccuracies, and may be incomplete or updated. Excelsior, therefore, reserves the right to correct any inaccuracies, errors, or omissions (comprising of after a program or an order has been submitted) and to modify or change information at any time without prior notice.
+<br/>Platforms, Programs, and any information or Excelsior Content are provided on an “as is” and “as available” basis.
+
+<br/>Excelsior does not make any representations or warranties of any kind, whether express or implied, concerning our content or services. Excelsior does not represent that the service will meet your requirements or that the service will be uninterrupted, timely, secure, or error-free; nor does Excelsior offer any warranty with respect to the results that may be obtained from the use of the service. As with any internet-based interaction, we recommend that you exercise discretion and caution.
+<br/>You should be aware that any information or material downloaded from our Platforms or Programs is done so at your own discretion and risk and that you will be solely responsible for any damage to your computer system or other device, the deletion of any data acquired, images viewed, or anything else that may happen as a result of such actions.
+<br/>Excelsior shall not be responsible for any advice or information, whether oral or written, obtained by the user through the service or through Excelsior.</p>
 			</div>
     </div>
   )
